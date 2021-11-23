94578c90fab2badf3eb400ae8f9e5ef01344bfb0
tejall <tejalgirme19@gmail.com>
2021-11-23 11:46:45 +0530

    update commit

--- main.go
+++ main.go
@@ -7,13 +7,21 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	
 
 	"github.com/gorilla/mux"
 )
 
-var Items =  []Item
+//Item Struct
+type Item struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+	Price    int    `json:"price"`
+}
 
+var Items []Item
+
+//Consuming API Endpoints
 var ApiUrls = []string{
 	"https://run.mocky.io/v3/c51441de-5c1a-4dc2-a44e-aab4f619926b",
 	"https://run.mocky.io/v3/4ec58fbc-e9e5-4ace-9ff0-4e893ef9663c",
@@ -29,17 +37,19 @@ func main() {
 	r.HandleFunc("/buy-item/{name}", getByName).Methods("GET")
 	r.HandleFunc("/buy-item-qty/{name}&{quantity}", getByQuantity).Methods("GET")
 	r.HandleFunc("/buy-item-qty-price/{name}&{quantity}&{price}", getByPrice).Methods("GET")
-	
+
 	fmt.Println("Server Started on 5000 port")
 	log.Fatal(http.ListenAndServe(":5000", r))
-	
+
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1> Welcome to Food Aggregator</h1>"))
 }
 
+//Get Items by Name
 func getByName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	flag := 0
 	for _, web := range ApiUrls {
 		res, err := http.Get(web)
@@ -53,8 +63,7 @@ func getByName(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
-		data := strClean(dataBytes)
-
+		data := dataBytes
 		json.Unmarshal(data, &Items)
 
 		params := mux.Vars(r)
@@ -74,7 +83,9 @@ func getByName(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Get Items by Name & Quantity
 func getByQuantity(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	flag := 0
 	for _, web := range ApiUrls {
 		res, err := http.Get(web)
@@ -88,8 +99,7 @@ func getByQuantity(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
-		data := strClean(dataBytes)
-
+		data := dataBytes
 		json.Unmarshal(data, &Items)
 
 		params := mux.Vars(r)
@@ -109,7 +119,10 @@ func getByQuantity(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("NOT_FOUND")
 	}
 }
+
+//Get Items by Name , Quantity & Price
 func getByPrice(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	flag := 0
 	for _, web := range ApiUrls {
 		res, err := http.Get(web)
@@ -123,17 +136,15 @@ func getByPrice(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
-		data := strClean(dataBytes)
-
+		data := dataBytes
 		json.Unmarshal(data, &Items)
 
 		params := mux.Vars(r)
-		qty, _ := strconv.Atoi(params["quantity"])
 		price, _ := strconv.Atoi(params["price"])
 
 		for _, item := range Items {
 			fmt.Println(item)
-			if item.Name == params["name"] && item.Quantity >= qty && item.Price >= price {
+			if item.Name == params["name"] && item.Price >= price {
 				flag = 1
 				json.NewEncoder(w).Encode(item)
 				return
@@ -145,6 +156,3 @@ func getByPrice(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("NOT_FOUND")
 	}
 }
-
-
-
